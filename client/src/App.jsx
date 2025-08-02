a84653f795964b56e540e50d2bc294f75792269d
Hanni Tin <bisakookjin@gmail.com>
2025-08-02 15:09:39 +0630

    Hanni 35:56

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import CreatePost from './pages/CreatePost'
+import Feed from './pages/Feed'
+import Messages from './pages/Messages'
+import ChatBox from './pages/ChatBox'
+import Discover from './pages/Discover'
+import Connections from './pages/Connections'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div className='text-xl text-red-500'>App</div>
+    <>
+      <Routes>
+        <Route path='/' element={<Login />}>
+          <Route index element={<Feed />} />
+          <Route path='messages' element={<Messages />} />
+          <Route path='messages/:userId' element={<ChatBox />} />
+          <Route path='connections' element={<Connections />} />
+          <Route path='discover' element={<Discover />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='profile/:profileId' element={<Profile />} />
+          <Route path='create-post' element={<CreatePost />} />
+        </Route>
+      </Routes>
+
+    </>
   )
 }
 
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import { Star } from 'lucide-react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      {/* Background Image */}
+      <img src={assets.bgImage} alt="" className='absolute top-0 left-0 -z-1 w-full h-full object-cover' />
+      {/* left side : Branding */}
+      <div className='flex-1 flex flex-col items-start justify-between p-6 md:p-10 lg:pl-40'>
+        <img src={assets.logo} alt="" className='h-12 object-contain' />
+        <div>
+        <div>
+        <img src={assets.group_users} alt="" className='h-8 md:h-10'/>
+        <div>
+          <div>
+            {Array(5).fill(0).map((_,i)=>(<Star key={i} className='size-4 md:size-4.5 text-transparent fill-amber-500'/>))}
+          </div>
+        </div>
+        </div>
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
