23bf7da8555fc5afd9aef521246fd01b1366fc91
Khant Nyi <khantnyinyi432001@gmail.com>
2025-08-02 11:37:21 +0630

    Khant1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>
+      <button onClick={() => alert('Button clicked!')}>Click Me</button>
+    </div>
   )
 }
 
