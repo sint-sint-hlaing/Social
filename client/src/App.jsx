2db321a82481bcbc41c330acfdfe076b3a0df3da
Sint Sint Hlaing <sints2001@gmail.com>
2025-08-02 11:40:51 +0630

    Sint 11:39am 8/2/2025

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Welcome to the React App</h1>
-      <p>This is a simple React application.</p>
-      <button onClick={() => alert('Button clicked!')}>Click Me</button>
+      <h1>Welcome   </h1>
+    
     </div>
   )
 }
