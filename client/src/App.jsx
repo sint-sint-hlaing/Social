2797c6ed36f7f5033d55d57db0216f5775e969c5
Yin Nyein Chan <yinnyeinchan01@gmail.com>
2025-08-02 12:07:09 +0630

    Yin 0:0 - 0 : 15

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div>
       <h1>Welcome hello  </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti nobis facere consequatur pariatur. Quidem rem at, fugit eos minima, tenetur numquam magni possimus accusamus quam dolorum optio commodi labore ea.</p>
     
     </div>
   )
