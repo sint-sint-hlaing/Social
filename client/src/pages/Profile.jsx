f97ae6f100724858e9d71a7f66d180b6b3524e90
Yin Nyein Chan <yinnyeinchan01@gmail.com>
2025-08-03 21:19:03 +0630

    Yin 4:03:40

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { dummyPostsData, dummyUserData } from '../assets/assets'
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+  const {ProfileId}= useParams()
+  const [user, setUser]= useState(null)
+  const [posts, setPosts]= useState([])
+  const [activeTab, setActiveTab]= useState('posts')
+  const [showEdit, setShowEdit]= useState('false')
+
+const fetchUser = async () => {
+  setUser(dummyUserData)
+  setPosts(dummyPostsData)
+}
+useEffect(()=>{
+  fetchUser()
+},[])
+  return user ? (
+    <div className='relative h-full overflow-y-scroll bg-gray-50 p-6'>
+
+    </div>
+  ) :(<loading />)
 }
 
 export default Profile
