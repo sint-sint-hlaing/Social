9e042a9c7ede2c66507fefa94dea5116346f6fb0
Khant Nyi <khantnyinyi432001@gmail.com>
2025-08-03 07:46:21 +0630

    khant 1:13:20

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -12,8 +12,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 
   return (
     <div
-      className={`w-60 xl:w-72  bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-0 bottom-0 x-20 ${
-        sidebarOpen ? "translae-x-0" : "max-sm:translate-x-full"
+      className={`w-60 xl:w-72  bg-white border-r border-gray-200 flex flex-col justify-between items-center max-sm:absolute top-0 bottom-0 x-20 
+        
+        ${sidebarOpen ? "translate-x-0" : "max-sm:translate-x-full"
       } transition-all duration-300 ease-in-out`}
     >
       <div className="w-full">
