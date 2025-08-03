b8219cda54d157cc4e959b943c7a8689f545a517
Sint Sint Hlaing <sints2001@gmail.com>
2025-08-03 15:46:02 +0630

    Sint 3:24:45

--- client/src/pages/Connections.jsx
+++ client/src/pages/Connections.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { UserCheck, UserPlus, UserRoundPen, Users } from "lucide-react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  dummyConnectionsData as connections ,
+  dummyFollowingData as following,
+  dummyFollowersData as followers,
+  dummyPendingConnectionsData as pending
+} from '../assets/assets'
 
 const Connections = () => {
+  const navigate = useNavigate();
+  const dataArray = [
+    { label: "Followers", value: followers, icon: Users },
+    { label: "Following", value: following, icon: UserCheck },
+    { label: "Pending", value: pending, icon: UserRoundPen },
+    { label: "Connections", value: connections, icon: UserPlus },
+  ];
   return (
-    <div>Connections</div>
-  )
-}
+    <div className="min-h-screen bg-slate-50">
+      <div className=" max-w-6xl mx-auto p-6">
+        {/* Title */}
+        <div className="mb-8">
+          <h1 className="text-3xl font-bold text-slate-900 mb-2 ">
+            Connections
+          </h1>
+          <p className="text-slate-600">
+            Manage your metwork and disover new connections
+          </p>
+        </div>
 
-export default Connections
+        {/* Counts */}
+        <div className=" mb-8 flex flex-wrap gap-6">
+          {dataArray.map((item, index) => (
+            <div key={index } className=" flex flex-col items-center justify-center gap-1 border h-20 w-40 border-gray-200 bg-white shadow rounded-md">
+              <b>{item.value.length}</b>
+              <p className="text-slate-600">{item.label}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Connections;
