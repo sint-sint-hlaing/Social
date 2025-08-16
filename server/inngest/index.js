238fa69f017932f577ba6ad5494abb145c41f501
Sint Sint Hlaing <sints2001@gmail.com>
2025-08-16 20:28:25 +0630

    third time delete inngest fun:

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -4,6 +4,7 @@ import Connection from "../models/Connection.js";
 import sendEmail from "../config/nodeMailer.js";
 import Story from "../models/Story.js";
 import Message from "../models/Message.js";
+import Post from "../models/Post.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({
