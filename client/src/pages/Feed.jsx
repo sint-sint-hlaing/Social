7035fe6a7f61c763b71110760b1f31a689059d26
Sint Sint Hlaing <sints2001@gmail.com>
2025-08-20 19:25:34 +0630

    remove sponser

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -136,13 +136,13 @@ const Feed = () => {
       </div>
 
       <div className="max-xl:hidden sticky top-0">
-        <div className="bg-white rounded-2xl shadow-md p-4 flex flex-col gap-4 w-64">
+        {/* <div className="bg-white rounded-2xl shadow-md p-4 flex flex-col gap-4 w-64">
           <h3 className="text-lg font-semibold text-gray-800 flex items-center gap-2">
             <BookOpen className="w-5 h-5 text-gray-700" />
             Recommended Resources
           </h3>
 
-          {/* Resource Item */}
+         
           {[
             {
               title: "Python Data Structures",
@@ -186,7 +186,7 @@ const Feed = () => {
           >
             See all resources →
           </a>
-        </div>
+        </div> */}
 
         <RecentMessages />
       </div>
