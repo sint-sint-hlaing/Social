ffaf10e4694e0c3d7986b6a7a069a818dbb996d7
Khant Nyi <khantnyinyi432001@gmail.com>
2025-08-26 14:28:18 +0630

    change color cyan

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -4,17 +4,17 @@ export default function Logo() {
   return (
     <div className="flex items-center space-x-1">
       {/* Logo Image */}
-      {/* <img
+      <img
         src={assets.logo}
         alt="Knowledge Hive Logo"
         className="w-14 h-14 object-contain"
-      /> */}
+      />
 
       {/* Text */}
-      <h3 className="font-extrabold text-3xl tracking-tight">
+      <h3 className="font-extrabold text-3xl tracking-wider ">
         Knowledge
         <br />
-        <span className="bg-gradient-to-r from-cyan-400 via-teal-500 to-blue-600 text-transparent bg-clip-text">
+        <span className="bg-gradient-to-r from-cyan-400 via-teal-500 to-blue-600 text-transparent bg-clip-text font-bold">
           Hive
         </span>
       </h3>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
       />
       <div className="bg-white/30 backdrop-blur-xs flex flex-col md:flex-row relative flex-1">
         {/* Dark overlay */}
-        {/* <div className="absolute top-0 left-0 -z-5 w-full h-full bg-black/40" /> */}
+        <div className="absolute top-0 left-0 -z-5 w-full h-full bg-black/30" />
 
         {/* Left side : Branding */}
         <div className=" flex-1 flex flex-col items-start justify-between p-6 md:p-10 lg:pl-32 relative z-10">
@@ -24,7 +24,7 @@ const Login = () => {
 
           {/* Hero Section */}
           <div className="mt-10">
-            <h1 className="text-4xl md:text-6xl font-extrabold  leading-tight text-[#212121]">
+            <h1 className="text-4xl md:text-6xl font-extrabold  leading-tight text-black tracking-wider">
               Share Knowledge, <br />
               <span className="bg-gradient-to-r from-cyan-400 via-teal-500 to-blue-600 text-transparent bg-clip-text">
                 Build Connections
@@ -33,7 +33,7 @@ const Login = () => {
             <span className=" text-2xl font-bold text-gray-100 drop-shadow-md">
               within Computer Students
             </span>
-            <p className="mt-4 text-lg md:text-xl text-gray-200 drop-shadow">
+            <p className="mt-4 text-lg md:text-xl text-gray-200 drop-shadow font-medium">
               Connect with classmates, exchange insights, and build lasting
               friendships on campus.
             </p>
